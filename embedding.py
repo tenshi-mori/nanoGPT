340d5a2621e59b65d77cf74825c08966de09cf83
tenshi-mori <tenshi.ciaco.mori@gmail.com>
2025-09-10 17:57:20 -0700

    Annotated the Tensors for better readability

--- embedding.py
+++ embedding.py
@@ -1,5 +1,3 @@
-import math
-from tkinter import S
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -8,7 +6,9 @@ from torchtune.modules import RotaryPositionalEmbeddings
 
 import einops
 from jaxtyping import Float
-
+'''
+Implementation based off of mine and Pytorch's. Still haven't figured out why my implementation is slightly different from this. 
+'''
 class RotaryPositionalEmbedding(nn.Module):
     def __init__(self, dim: int, base: int = 10000):
         super().__init__()
@@ -23,16 +23,16 @@ class RotaryPositionalEmbedding(nn.Module):
 
         theta = 1.0 / (self.base ** (torch.arange(0, self.dim, 2).float() / self.dim))
         seq_idx = torch.arange(t).float()
-        idx_theta = einops.einsum(seq_idx, theta, "s, d_half -> s d_half")
+        idx_theta: Float[Tensor, "s d_half"] = einops.einsum(seq_idx, theta, "s, d_half -> s d_half")
 
         cache = torch.stack([torch.cos(idx_theta), torch.sin(idx_theta)], dim=-1)
-        self.cache = cache[None, :, None, :, :]
+        self.cache: Float[Tensor, "1 t 1 dh//2 2"] = cache[None, :, None, :, :]
 
     def forward(self, x: Float[Tensor, "b t nh dh"]) -> Float[Tensor, "b t nh dh"]:
         t = x.shape[1]
-        self._build_cache(x.shape[1])
-        xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
-        rope_cache = self.cache[:, :t, :, :, :]
+        self._build_cache(t)
+        xshaped: Float[Tensor, "b t nh dh//2 2"] = x.float().reshape(*x.shape[:-1], -1, 2)
+        rope_cache: Float[Tensor, "1 t 1 dh//2 2"] = self.cache[:, :t, :, :, :]
 
         x_out = torch.stack(
             [
@@ -44,7 +44,7 @@ class RotaryPositionalEmbedding(nn.Module):
             -1,
         )
 
-        x_out = x_out.flatten(3)
+        x_out: Float[Tensor, "b t nh dh"] = einops.rearrange(x_out, "b t nh ... -> b t nh (...)")
         return x_out.type_as(x)
 
 if __name__ == "__main__":
@@ -55,9 +55,6 @@ if __name__ == "__main__":
     n_heads = 8
     d_head = 8
 
-
-    # rope = RotaryPositionalEmbedding(d_model)
-
     x = torch.randn(batch, seq_len, n_heads, d_head, dtype=float)
 
     test_RoPE = RotaryPositionalEmbeddings(d_head)
@@ -65,8 +62,6 @@ if __name__ == "__main__":
 
     out = RoPE(x)
     test_out = test_RoPE(x)
-    print(out.shape)
-    print(test_out.shape)
     torch.testing.assert_close(test_out, out)
     print("Passed Assertion test against Pytorch RoPE")
 
