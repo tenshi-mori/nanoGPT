184dfb1f540ec96c1beb4e4176af14bdf3e0df45
tenshi-mori <tenshi.ciaco.mori@gmail.com>
2025-09-10 17:29:23 -0700

    Just checked the implementation in Pytorch, seems to work better

--- embedding.py
+++ embedding.py
@@ -10,36 +10,42 @@ import einops
 from jaxtyping import Float
 
 class RotaryPositionalEmbedding(nn.Module):
-    def __init__(self, d: int, base: int = 10000):
+    def __init__(self, dim: int, base: int = 10000):
         super().__init__()
 
         self.base = base
-        self.d = d
-        self.cos_cached = None
-        self.sin_cached = None
+        self.dim = dim
+        self.cache = None
 
     def _build_cache(self, t: int) -> None:
-        if self.cos_cached is not None and t <= self.cos_cached.shape[1]:
+        if self.cache is not None and t <= self.cache.shape[1]:
             return 
 
-        theta = 1.0 / (self.base ** (torch.arange(0, self.d, 2).float() / self.d))
+        theta = 1.0 / (self.base ** (torch.arange(0, self.dim, 2).float() / self.dim))
         seq_idx = torch.arange(t).float()
         idx_theta = einops.einsum(seq_idx, theta, "s, d_half -> s d_half")
-        idx_theta2 = torch.cat([idx_theta, idx_theta], dim=1)
 
-        self.cos_cached = idx_theta2.cos()[None, :, None, :]
-        self.sin_cached = idx_theta2.sin()[None, :, None, :]
-
-    def _neg_half(self, x: Float[Tensor, "b t nh dh"]) -> Float[Tensor, "b t nh dh"]:
-        d_2 = self.d // 2
-        return torch.cat([-x[:, :, :, d_2:], x[:, :, :, :d_2]], dim = -1)
+        cache = torch.stack([torch.cos(idx_theta), torch.sin(idx_theta)], dim=-1)
+        self.cache = cache[None, :, None, :, :]
 
     def forward(self, x: Float[Tensor, "b t nh dh"]) -> Float[Tensor, "b t nh dh"]:
         t = x.shape[1]
         self._build_cache(x.shape[1])
-        neg_half_x = self._neg_half(x)
-        x_rope = (x * self.cos_cached[:, :t, :, :]) + (neg_half_x * self.sin_cached[:, :t, :, :])
-        return x_rope
+        xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
+        rope_cache = self.cache[:, :t, :, :, :]
+
+        x_out = torch.stack(
+            [
+                xshaped[..., 0] * rope_cache[..., 0]
+                - xshaped[..., 1] * rope_cache[..., 1],
+                xshaped[..., 1] * rope_cache[..., 0]
+                + xshaped[..., 0] * rope_cache[..., 1],
+            ],
+            -1,
+        )
+
+        x_out = x_out.flatten(3)
+        return x_out.type_as(x)
 
 if __name__ == "__main__":
     # Test RotaryPositionalEmbedding
